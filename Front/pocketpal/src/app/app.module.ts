c62b43d3d7f0c95145dba6e174c1a4c7519d1781
Firaol Ibrahim <firaolibrahim28@gmail.com>
2023-04-19 11:51:56 +0300

    some trials on the login service

--- Front/pocketpal/src/app/app.module.ts
+++ Front/pocketpal/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -8,6 +9,7 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
 import { AIpageComponent } from './aipage/aipage.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AppserviceService } from './appservice.service';
 
 @NgModule({
   declarations: [
@@ -17,8 +19,13 @@ import { RouterModule, Routes } from '@angular/router';
     AIpageComponent,
     LandingPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [AppserviceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Front/pocketpal/src/app/appservice.service.ts
+++ Front/pocketpal/src/app/appservice.service.ts
@@ -1,53 +1,70 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppserviceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  signUp(name: string, password: string, email: string){
-
-    let body = {name: name, password: password, email: email}
-    return this.http.post("http://Pocket-pal-api.vercel.app/signup", body)
-  }
-  
-  signIn(email: string, password: string){
-    let body = {email: email, password: password}
-    return this.http.post("http://Pocket-pal-api.vercel.app/signin", body)
-  }
-  
-  getProfile(token: string){
-    const header = new HttpHeaders({'Authorization': `Bearer ${token}`});
-    return this.http.get("http://Pocket-pal-api.vercel.app/profile",{headers: header})
-  }
-  
-  expenses(token: string, month: any, year: any, name:string, amount: number, category: string){
-    const header = new HttpHeaders({'Authorization': `Bearer ${token}`});
-    let body = {name: name, amount: amount, category: category}
-    return this.http.post(`http://Pocket-pal-api.vercel.app,body/budgets/${month}/${year}/expenses`, {headers: header})
-  }
-  
-  getBudget(token: string, month: number, year: number, balance: string){
-    const header = new HttpHeaders({'Authorization': `Bearer ${token}`});
-    let body = {month: month, year: year, balance: balance}
-    return this.http.post("http://Pocket-pal-api.vercel.app/budgets", body, {headers: header})
+  signUp(name: string, password: string, email: string) {
+    let body = { name: name, password: password, email: email };
+    return this.http.post('http://Pocket-pal-api.vercel.app/signup', body);
   }
 
-  deleteExpense(token: string, budget_index: any, expense_index: any){
-    const header = new HttpHeaders({'Authorization': `Bearer ${token}`});
-    return this.http.delete(`http://Pocket-pal-api.vercel.app/budget/${budget_index}/expenses/${expense_index}`, {headers: header})
+  signIn(email: string, password: string) {
+    let body = { email: email, password: password };
+    return this.http.post('https://Pocket-pal-api.vercel.app/signin', body);
   }
-  
-  deletBudget(token: string, budget_index: any){
-    const header = new HttpHeaders({'Authorization': `Bearer ${token}`});
-    return this.http.delete(`http://Pocket-pal-api.vercel.app/budget/${budget_index}`, {headers: header})
+
+  getProfile(token: string) {
+    const header = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.get('http://Pocket-pal-api.vercel.app/profile', {
+      headers: header,
+    });
   }
-  
-  getAIprompt(prompt: string){
-    return this.http.get(`http://Pocket-pal-api.vercel.app/${prompt}`)
+
+  expenses(
+    token: string,
+    month: any,
+    year: any,
+    name: string,
+    amount: number,
+    category: string
+  ) {
+    const header = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    let body = { name: name, amount: amount, category: category };
+    return this.http.post(
+      `http://Pocket-pal-api.vercel.app,body/budgets/${month}/${year}/expenses`,
+      { headers: header }
+    );
   }
-  
+
+  getBudget(token: string, month: number, year: number, balance: string) {
+    const header = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    let body = { month: month, year: year, balance: balance };
+    return this.http.post('http://Pocket-pal-api.vercel.app/budgets', body, {
+      headers: header,
+    });
+  }
+
+  deleteExpense(token: string, budget_index: any, expense_index: any) {
+    const header = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.delete(
+      `http://Pocket-pal-api.vercel.app/budget/${budget_index}/expenses/${expense_index}`,
+      { headers: header }
+    );
+  }
+
+  deletBudget(token: string, budget_index: any) {
+    const header = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.delete(
+      `http://Pocket-pal-api.vercel.app/budget/${budget_index}`,
+      { headers: header }
+    );
+  }
+
+  getAIprompt(prompt: string) {
+    return this.http.get(`http://Pocket-pal-api.vercel.app/${prompt}`);
+  }
 }
