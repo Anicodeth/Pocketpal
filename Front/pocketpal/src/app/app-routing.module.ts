ed33c48fcddf9aa2255434101f5c7cc432d9673f
Ananya <afmtrendaid@gmail.com>
2023-04-19 00:29:19 -0700

    minor fixes

--- Front/pocketpal/src/app/app-routing.module.ts
+++ Front/pocketpal/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'loginPage', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
